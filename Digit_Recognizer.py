cc7b17c1abeb026a81b6b90293072581fe15592b
Eric Zhou <ericz1803@gmail.com>
2018-02-10 10:08:57 -0800

    Put preprocessing steps into a function so fthe user can write multiple digits each time it's run

--- Digit_Recognizer.py
+++ Digit_Recognizer.py
@@ -7,40 +7,46 @@ import matplotlib.pyplot as plt
 #get user image
 import get_number_pixels
 
-cwd = os.getcwd()
+global cwd
+cwd=os.getcwd()
 
-get_number_pixels.get_pixels()
-#read in image
-pic=cv2.imread(os.path.join(cwd,"screenshot.png"))
-os.remove(os.path.join(cwd,"screenshot.png"))
-#convert it into grayscale
-pic=cv2.cvtColor(pic,cv2.COLOR_BGR2GRAY)
-#resize to 28x28 (mnist size) using opencv anti-aliasing
-pic=cv2.resize(pic, (28,28), fx=0.5, fy=0.5, interpolation = cv2.INTER_AREA)
-plt.imshow(pic, cmap='gray')
-plt.title('Image after anti-aliasing and changing the size to 28x28px.')
-plt.show()
-#preprocess(translate it to center of mass)
-intensity_sum=np.sum(pic)
-pic=np.array(pic)
-x_sum=0
-y_sum=0
-for x in range(pic.shape[0]):
-    for y in range(pic.shape[1]):
-        x_sum+=x*pic[x,y]
-        y_sum+=y*pic[x,y]
-x_sum=np.round(x_sum/intensity_sum)
-y_sum=np.round(y_sum/intensity_sum)
+def get_img_and_preprocess(show=True):
+    get_number_pixels.get_pixels()
+    #read in image
+    pic=cv2.imread(os.path.join(cwd,"screenshot.png"))
+    os.remove(os.path.join(cwd,"screenshot.png"))
+    #convert it into grayscale
+    pic=cv2.cvtColor(pic,cv2.COLOR_BGR2GRAY)
+    #resize to 28x28 (mnist size) using opencv anti-aliasing
+    pic=cv2.resize(pic, (28,28), fx=0.5, fy=0.5, interpolation = cv2.INTER_AREA)
+    if show:
+        plt.imshow(pic, cmap='gray')
+        plt.title('Image after anti-aliasing and changing the size to 28x28px.')
+        plt.show()
+    #preprocess(translate it to center of mass)
+    intensity_sum=np.sum(pic)
+    pic=np.array(pic)
+    x_sum=0
+    y_sum=0
+    for x in range(pic.shape[0]):
+        for y in range(pic.shape[1]):
+            x_sum+=x*pic[x,y]
+            y_sum+=y*pic[x,y]
+    x_sum=np.round(x_sum/intensity_sum)
+    y_sum=np.round(y_sum/intensity_sum)
 
-#M is translation matrix
-M = np.float32([[1,0,14-y_sum],[0,1,14-x_sum]])
-pic = cv2.warpAffine(pic, M, (28, 28))
-plt.imshow(pic, cmap='gray')
-plt.title('After Translation')
-plt.show()
-#flatten it and normalize values
-pic=pic.reshape(1,784)/255
+    #M is translation matrix
+    M = np.float32([[1,0,14-y_sum],[0,1,14-x_sum]])
+    pic = cv2.warpAffine(pic, M, (28, 28))
+    if show:
+        plt.imshow(pic, cmap='gray')
+        plt.title('After Translation')
+        plt.show()
+    #flatten it and normalize values
+    pic=pic.reshape(1,784)/255
 
+    #returns 28x28 numpy array
+    return pic
 
 nodes_hl1=1000
 nodes_hl2=500
@@ -95,4 +101,10 @@ with tf.Session() as sess:
         print("Model restored.")
     except:
         print("Error: No save file found")
-    print("Prediction: {}".format(sess.run(prediction, feed_dict={features: pic})[0]))
+    
+    enter_another_img='yes'
+    while enter_another_img.lower()[0]=='y':
+        pic=get_img_and_preprocess(show=False)
+        print("Prediction: {}".format(sess.run(prediction, feed_dict={features: pic})[0]))
+        enter_another_img=input("Draw another number? (y/n)")
+
